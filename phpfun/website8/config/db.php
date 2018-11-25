83f40437a759ed920b1bc0062c2291c2d4ee21b1
ntnikhilesh <ntnikhilesh@gmail.com>
2018-11-26 00:03:49 +0530

    DB connection and fetch data

--- phpfun/website8/config/db.php
+++ phpfun/website8/config/db.php
@@ -1,7 +1,7 @@
 <?php
 
 // create connection
-$conn = mysqli_connect('localhost', 'root', '12345', 'phpblog');
+$conn = mysqli_connect('localhost', 'root', '', 'phpblog');
 
 //check connection
 if(mysqli_connect_errno()){
